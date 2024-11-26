9032afbd6b818b8adc14a5a46ed43a505dddd543
Sachin Gupta <120103804+SachinXCode313@users.noreply.github.com>
2024-11-26 23:17:08 +0530

    Create Day 12 - Max Circular Subarray Sum.java

---
+++ Day 12 - Max Circular Subarray Sum.java
@@ -0,0 +1,26 @@
+public int circularSubarraySum(int arr[]) {
+        int totalSum = 0;    
+        int currMaxSum = 0, currMinSum = 0;
+        int maxSum = arr[0], minSum = arr[0];
+        
+        for(int i = 0; i < arr.length; i++) {
+          
+            currMaxSum = Math.max(currMaxSum + arr[i], arr[i]);
+            maxSum = Math.max(maxSum, currMaxSum); 
+          
+    
+            currMinSum = Math.min(currMinSum + arr[i], arr[i]);
+            minSum = Math.min(minSum, currMinSum);
+            
+
+            totalSum += arr[i];
+        }
+        
+        int normalSum = maxSum;
+        int circularSum = totalSum - minSum;
+        
+        if(minSum == totalSum)
+            return normalSum;
+        
+        return Math.max(normalSum, circularSum);
+    }
